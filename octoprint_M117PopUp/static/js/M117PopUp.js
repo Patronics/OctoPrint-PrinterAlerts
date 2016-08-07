2581098bc1fad142714121f0aec003437420b411
jneilliii <jneilliii+github@gmail.com>
2016-08-06 22:07:43 -0400

    Another attempt.

--- octoprint_M117PopUp/static/js/M117PopUp.js
+++ octoprint_M117PopUp/static/js/M117PopUp.js
@@ -10,7 +10,7 @@ $(function() {
 			
 			if(data.type == "popup") {
 				console.log(data.msg);
-				self.popup = new PNotify({
+				new PNotify({
 					title: 'M117 Pop Up Message',
 					text: data.msg
 					});
@@ -21,16 +21,16 @@ $(function() {
 
     // This is how our plugin registers itself with the application, by adding some configuration
     // information to the global variable OCTOPRINT_VIEWMODELS
-    OCTOPRINT_VIEWMODELS.push([
+    ADDITIONAL_VIEWMODELS.push([
         // This is the constructor to call for instantiating the plugin
         M117PopUpViewModel,
 
         // This is a list of dependencies to inject into the plugin, the order which you request
         // here is the order in which the dependencies will be injected into your view model upon
         // instantiation via the parameters argument
-        [],
+        ["loginStateViewModel"],
 
         // Finally, this is the list of selectors for all elements we want this view model to be bound to.
-        ["#sidebar_plugin_M117PopUp"]
+        document.getElementById('sidebar_plugin_M117PopUp')
     ]);
 });
