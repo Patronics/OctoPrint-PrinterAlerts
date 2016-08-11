9ff438aef2faf5e1344280ced9dfbe2f2bf3d96b
jneilliii <jneilliii+github@gmail.com>
2016-08-11 19:42:50 -0400

    Fixing reference to cmd

--- octoprint_M117PopUp/__init__.py
+++ octoprint_M117PopUp/__init__.py
@@ -4,8 +4,8 @@ import octoprint.plugin
 
 class M117PopUp(octoprint.plugin.AssetPlugin):
 	def AlertM117(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
-		if gcode and gcode.startswith("M117"):
-			self._plugin_manager.send_plugin_message(self._identifier, dict(type="popup", msg=gcode.replace("M117 ","")))
+		if gcode and cmd.startswith("M117"):
+			self._plugin_manager.send_plugin_message(self._identifier, dict(type="popup", msg=cmd.replace("M117 ","")))
 			return
 			
 	def get_assets(self):
