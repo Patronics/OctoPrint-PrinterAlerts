1c60ab98f407ddd4cd14f61cc5f25ab3d8418266
jneilliii <jneilliii+github@gmail.com>
2016-09-25 02:53:58 -0400

    Fixing default settings.

--- octoprint_M117PopUp/__init__.py
+++ octoprint_M117PopUp/__init__.py
@@ -18,7 +18,7 @@ class M117PopUp(octoprint.plugin.AssetPlugin,
 		
 	##-- Settings hooks
 	def get_settings_defaults(self):
-		return dict(msgType="notice",autoClose=true)	
+		return dict(msgType="notice",autoClose=True)	
 	
 	##-- Template hooks
 	def get_template_configs(self):
