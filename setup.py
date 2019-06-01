7d6aa4b1caf1e7f19ac8ad8b98867c0a3261f0a9
Patrick Leiser <patrick27leiser@yahoo.com>
2019-06-01 14:27:31 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,30 +4,30 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "M117PopUp"
+plugin_identifier = "PrinterAlerts"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
 plugin_package = "octoprint_%s" % plugin_identifier
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-M117PopUp"
+plugin_name = "OctoPrint-PrinterAlerts"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "0.6.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = "Simple plugin that sends M117 messages in gcode to the web interface."
+plugin_description = "Simple plugin that provides  alerts of 'Printer waiting for user interaction' to the web interface."
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "jneilliii"
+plugin_author = "Patrick Leiser"
 
 # The plugin's author's mail address.
-plugin_author_email = "jneilliii+octoprint@gmail.com"
+plugin_author_email = "patrick27leiser@yahoo.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/jneilliii/OctoPrint-M117PopUp"
+plugin_url = "https://github.com/Patronics/Octoprint-PrinterAlerts"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
